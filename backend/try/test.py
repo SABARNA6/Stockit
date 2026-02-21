dc8db21e4ba8e70ff291bb50f63582e013355c9b
sabarna6 <sabarnakrishbalu@gmail.com>
2026-02-21 12:24:08 +0530

    cleaned Unnessary files

--- backend/try/test.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import yfinance.base
-import nsepython as nse
-yff=yfinance.ticker.Ticker("TCS")
-# print(yf.get_news())
-ticer = yf.ticker.Ticker("TCS")
-info = ticer.info
-# print(info)
-print(nse.index_info("TCS"))
